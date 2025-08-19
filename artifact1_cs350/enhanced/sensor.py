b4fb4014e93600537b337d6b67270b41cfc7def9
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-19 14:41:01 +0500

    Update sensor.py

--- artifact1_cs350/enhanced/sensor.py
+++ artifact1_cs350/enhanced/sensor.py
@@ -1,1 +1,12 @@
+import board, busio, adafruit_ahtx0
+
+class AHT20Sensor:
+    def __init__(self):
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self._dev = adafruit_ahtx0.AHTx0(i2c)
+
+    def read_f(self) -> float:
+        # AHT20 returns Celsius
+        c = float(self._dev.temperature)
+        return c * 9.0 / 5.0 + 32.0
 
