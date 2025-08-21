3f6fc2037ec5b1c908a67a28d87dd5ead679c8bb
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-21 21:44:38 +0500

    Update main.ts

--- artifact2_cs465/enhanced/client/src/main.ts
+++ artifact2_cs465/enhanced/client/src/main.ts
@@ -1,1 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/interceptors/auth.interceptor';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptors([authInterceptor]))
+  ]
+});
 
