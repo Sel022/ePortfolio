faef03f907543cac7154831e31b0c875b1d0678b
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-21 18:06:25 +0500

    Update validate.js

--- artifact2_cs465/enhanced/server/middleware/validate.js
+++ artifact2_cs465/enhanced/server/middleware/validate.js
@@ -1,1 +1,13 @@
+const { validationResult } = require('express-validator');
+
+function validateOrFail(req, res, next) {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    res.status(400);
+    return next(new Error(result.array().map(e => e.msg).join('; ')));
+  }
+  next();
+}
+
+module.exports = { validateOrFail };
 
