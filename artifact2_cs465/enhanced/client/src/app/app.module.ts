ee709f5142348eebc19ce62438157ac5df64e9f8
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-21 21:46:31 +0500

    Update app.module.ts

--- artifact2_cs465/enhanced/client/src/app/app.module.ts
+++ artifact2_cs465/enhanced/client/src/app/app.module.ts
@@ -1,1 +1,11 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './app/interceptors/auth.interceptor';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ]
+});
 
