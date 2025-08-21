49595d92fda2c719601fee08a571798f0ba8a5ab
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-21 18:07:43 +0500

    Update Trip.js

--- artifact2_cs465/enhanced/server/models/Trip.js
+++ artifact2_cs465/enhanced/server/models/Trip.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  slug:  { type: String, required: true, unique: true, index: true },
+  destination: { type: String, required: true, index: true },
+  startDate: { type: Date, required: true, index: true },
+  endDate:   { type: Date, required: true },
+  price: { type: Number, required: true, min: 0 },
+  description: { type: String, required: true, maxlength: 2000 },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+tripSchema.pre('save', function(next) {
+  if (this.endDate < this.startDate) {
+    return next(new Error('End date cannot be earlier than start date'));
+  }
+  next();
+});
+
+module.exports = mongoose.model('Trip', tripSchema);
 
