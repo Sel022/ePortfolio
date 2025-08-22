fe16e7b3520346ced77ff04c29d0a722c5e2525b
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-22 15:19:58 +0500

    Delete artifact1_cs350/enhanced/db_config.py

--- artifact1_cs350/enhanced/db_config.py
+++
@@ -1,9 +0,0 @@
-# db_config.py
-from pymongo import MongoClient
-
-def get_db():
-    # Replace with your MongoDB connection string
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["thermostat_db"]
-    return db
-
