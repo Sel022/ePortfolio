e7f4c1883468fe70a086fa5b6d5b3aa82b1be358
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-22 15:16:13 +0500

    Update logger.py

--- artifact1_cs350/enhanced/logger.py
+++ artifact1_cs350/enhanced/logger.py
@@ -1,1 +1,17 @@
+# logger.py
+import time
+from db_config import get_db
+
+db = get_db()
+logs = db["logs"]
+
+def write_log(state, temp, setpoint):
+    log_entry = {
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "state": state,
+        "temperature": temp,
+        "setpoint": setpoint
+    }
+    logs.insert_one(log_entry)
+    print(f"[DB LOGGED] {log_entry}")
 
