ada4574b109233f57ca50817d6930b60926357b2
Selva <167752582+Sel022@users.noreply.github.com>
2025-08-22 22:46:47 +0500

    Update User.js

--- artifact2_cs465/enhanced/server/database/models/User.js
+++ artifact2_cs465/enhanced/server/database/models/User.js
@@ -1,1 +1,23 @@
+// server/models/user.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, index: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+}, { timestamps: true });
+
+// Password hashing middleware
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare passwords
+userSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
